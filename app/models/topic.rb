b9e0ae067799be74f4b3acde71c57a71219f7458
Pedro Matiello <pmatiello@gmail.com>
2010-03-19 17:05:47 -0300

    10 elements/page.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -12,23 +12,23 @@ class Topic < ActiveRecord::Base
 
   # creator of forum topic
   belongs_to :user
-  
+
   # creator of recent post
   belongs_to :last_user, :class_name => "User"
-  
+
   belongs_to :forum, :counter_cache => true
-  
+
   # forum's site, set by callback
   belongs_to :site, :counter_cache => true
 
   has_many :posts,       :order => "#{Post.table_name}.created_at", :dependent => :delete_all
   has_one  :recent_post, :order => "#{Post.table_name}.created_at DESC", :class_name => "Post"
-  
+
   has_many :voices, :through => :posts, :source => :user, :uniq => true
-  
+
   has_many :monitorships, :dependent => :delete_all
   has_many :monitoring_users, :through => :monitorships, :source => :user, :conditions => {"#{Monitorship.table_name}.active" => true}
-  
+
   validates_presence_of :user_id, :site_id, :forum_id, :title
   validates_presence_of :body, :on => :create
 
@@ -36,7 +36,7 @@ class Topic < ActiveRecord::Base
   attr_accessible :title, :body
 
   attr_readonly :posts_count, :hits
-  
+
   has_permalink :title, :scope => :forum_id
 
 
@@ -47,7 +47,7 @@ class Topic < ActiveRecord::Base
   def sticky?
     sticky == 1
   end
-  
+
   def hit!
     self.class.increment_counter :hits, id
   end
@@ -55,11 +55,11 @@ class Topic < ActiveRecord::Base
   def paged?
     posts_count > Post.per_page
   end
-  
+
   def self.per_page
-  		2
-  end   
-  
+  		10
+  end
+
   def last_page
     [(posts_count.to_f / Post.per_page.to_f).ceil.to_i, 1].max
   end
@@ -67,13 +67,13 @@ class Topic < ActiveRecord::Base
   def update_cached_post_fields(post)
     # these fields are not accessible to mass assignment
     if remaining_post = post.frozen? ? recent_post : post
-      self.class.update_all(['last_updated_at = ?, last_user_id = ?, last_post_id = ?, posts_count = ?', 
+      self.class.update_all(['last_updated_at = ?, last_user_id = ?, last_post_id = ?, posts_count = ?',
         remaining_post.created_at, remaining_post.user_id, remaining_post.id, posts.count], ['id = ?', id])
     else
       destroy
     end
   end
-  
+
   def to_param
     permalink
   end
@@ -83,7 +83,7 @@ protected
     user.reply self, @body #unless locked?
     @body = nil
   end
-  
+
   def set_default_attributes
     self.site_id           = forum.site_id if forum_id
     self.sticky          ||= 0
@@ -102,11 +102,11 @@ protected
     Forum.update_all "posts_count = posts_count - #{posts_count}", ['id = ?', @old_forum_id]
     Forum.update_all "posts_count = posts_count + #{posts_count}", ['id = ?', forum_id]
   end
-  
+
   def count_user_posts_for_counter_cache
     @user_posts = posts.group_by { |p| p.user_id }
   end
-  
+
   def update_cached_forum_and_user_counts
     Forum.update_all "posts_count = posts_count - #{posts_count}", ['id = ?', forum_id]
     Site.update_all  "posts_count = posts_count - #{posts_count}", ['id = ?', site_id]
