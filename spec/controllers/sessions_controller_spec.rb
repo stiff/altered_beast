acd94397c59106e33b44c78693018afd97b8f36d
Pedro Matiello <pmatiello@gmail.com>
2010-03-23 17:26:40 -0300

    Fixing tests.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,29 +9,30 @@ describe SessionsController, "POST /create" do
       :pass    => 'test',
       :options => {}}
   end
-  
+
   act! { post :create, @login[:options].merge(:login => users(@login[:user]).login, :password => @login[:pass]) }
 
   # I think the session is broken. Or something.
   it_assigns :flash => {:notice => :not_nil}#, :session => {:user => :not_nil}
   it_redirects_to { '/' }
-  
+
   it 'fails login and does not redirect' do
     attempt_login 'bad password'
     session[:user].should be_nil
-    response.should be_success
+    response.should_not be_success
+    response.cookies["auth_token"].should be_nil
   end
 
   it 'remembers me' do
     attempt_login :remember_me => '1'
     response.cookies["auth_token"].should_not be_nil
   end
-  
+
   it 'does not remember me' do
     attempt_login :remember_me => '0'
     response.cookies["auth_token"].should be_nil
   end
-  
+
   def attempt_login(user = nil, password = nil, options = {})
     case user
       when Hash
@@ -52,7 +53,7 @@ describe SessionsController, "DELETE /destroy" do
 
   before { login_as :default }
   act! { get :destroy }
-  
+
   it_assigns :session => {:user => nil}
   it_redirects_to { '/' }
 
@@ -70,14 +71,14 @@ describe SessionsController, "(cookies)" do
     get :new
     controller.send(:logged_in?).should be_true
   end
-  
+
   it 'fails expired cookie login' do
     users(:default).remember_me_until 5.minutes.ago.utc
     request.cookies["auth_token"] = cookie_for(:default)
     get :new
     controller.send(:logged_in?).should_not be_true
   end
-  
+
   it 'fails cookie login' do
     users(:default).remember_me
     request.cookies["auth_token"] = auth_token('invalid_auth_token')
@@ -88,7 +89,7 @@ describe SessionsController, "(cookies)" do
   def auth_token(token)
     CGI::Cookie.new('name' => 'auth_token', 'value' => token)
   end
-    
+
   def cookie_for(user)
     auth_token users(user).remember_token
   end
