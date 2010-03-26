3f41bab88a9e938790e4ea89ab728b996b1fe9fa
Pedro Matiello <pmatiello@gmail.com>
2010-03-26 16:11:59 -0300

    SiteController tests passing.

--- app/helpers/path_helper.rb
+++ app/helpers/path_helper.rb
@@ -52,4 +52,8 @@ module PathHelper
     root_url
   end
 
+  def forums_url(*args)
+    root_url
+  end
+
 end
--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -9,13 +9,13 @@ describe SitesController, "GET #index" do
     @controller.stub!(:admin_required).and_return(true)
     @controller.stub!(:require_site)
   end
-  
+
   it_assigns :sites
   it_renders :template, :index
-  
+
   describe SitesController, "(xml)" do
     define_models :sites_controller
-    
+
     act! { get :index, :format => 'xml' }
 
     it_assigns :sites
@@ -33,13 +33,13 @@ describe SitesController, "GET #show" do
     @controller.stub!(:admin_required).and_return(true)
     @controller.stub!(:require_site)
   end
-  
+
   it_assigns :site
   it_renders :template, :show
-  
+
   describe SitesController, "(xml)" do
     define_models :sites_controller
-    
+
     act! { get :show, :id => @site.to_param, :format => 'xml' }
 
     it_renders :xml
@@ -59,15 +59,15 @@ describe SitesController, "GET #new" do
     act!
     assigns[:site].should be_new_record
   end
-  
+
   it "assigns current host to new @site" do
     request.host = "my.host"
     act!
     assigns[:site].host.should == "my.host"
   end
-  
+
   it_renders :template, :new
-  
+
   describe SitesController, "(xml)" do
     define_models :sites_controller
     act! { get :new, :format => 'xml' }
@@ -80,7 +80,7 @@ end
 describe SitesController, "GET #edit" do
   define_models :sites_controller
   act! { get :edit, :id => @site.to_param }
-  
+
   before do
     login_as :default
     @site  = sites(:default)
@@ -98,19 +98,19 @@ describe SitesController, "POST #create" do
     @attributes = {:name => 'yow'}
     @controller.stub!(:admin_required).and_return(true)
   end
-  
+
   describe SitesController, "(successful creation)" do
     define_models :sites_controller
     act! { post :create, :site => @attributes }
-    
+
     it_assigns :site, :flash => { :notice => :not_nil }
     it_redirects_to { site_path(assigns(:site)) }
   end
-  
+
   describe SitesController, "(successful creation, xml)" do
     define_models :sites_controller
     act! { post :create, :site => @attributes, :format => 'xml' }
-    
+
     it_assigns :site, :headers => { :Location => lambda { site_url(assigns(:site)) } }
     it_renders :xml, :status => :created
   end
@@ -122,11 +122,11 @@ describe SitesController, "POST #create" do
     it_assigns :site
     it_renders :template, :new
   end
-  
+
   describe SitesController, "(unsuccessful creation, xml)" do
     define_models :sites_controller
     act! { post :create, :site => {:name => ''}, :format => 'xml' }
-    
+
     it_assigns :site
     it_renders :xml, :status => :unprocessable_entity
   end
@@ -139,19 +139,19 @@ describe SitesController, "PUT #update" do
     @controller.stub!(:admin_required).and_return(true)
     @controller.stub!(:require_site)
   end
-  
+
   describe SitesController, "(successful save)" do
     define_models :sites_controller
     act! { put :update, :id => @site.to_param, :site => {} }
-    
+
     it_assigns :site, :flash => { :notice => :not_nil }
     it_redirects_to { site_path(@site) }
   end
-  
+
   describe SitesController, "(successful save, xml)" do
     define_models :sites_controller
     act! { put :update, :id => @site.to_param, :site => {}, :format => 'xml' }
-    
+
     it_assigns :site
     it_renders :blank
   end
@@ -159,15 +159,15 @@ describe SitesController, "PUT #update" do
   describe SitesController, "(unsuccessful save)" do
     define_models :sites_controller
     act! { put :update, :id => @site.to_param, :site => {:name => ''} }
-    
+
     it_assigns :site
     it_renders :template, :edit
   end
-  
+
   describe SitesController, "(unsuccessful save, xml)" do
     define_models :sites_controller
     act! { put :update, :id => @site.to_param, :site => {:name => ''}, :format => 'xml' }
-    
+
     it_assigns :site
     it_renders :xml, :status => :unprocessable_entity
   end
@@ -176,7 +176,7 @@ end
 describe SitesController, "DELETE #destroy" do
   define_models :sites_controller
   act! { delete :destroy, :id => @site.to_param }
-  
+
   before do
     login_as :default
     @site = sites(:default)
@@ -186,7 +186,7 @@ describe SitesController, "DELETE #destroy" do
 
   it_assigns :site
   it_redirects_to { sites_path }
-  
+
   describe SitesController, "(xml)" do
     define_models :sites_controller
     act! { delete :destroy, :id => @site.to_param, :format => 'xml' }
