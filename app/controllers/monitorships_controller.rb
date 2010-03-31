1fc1e037b6551ccfb2c434c72c32a91441165784
Pedro Matiello <pmatiello@gmail.com>
2010-03-31 13:39:12 -0300

    Enabling/disabling topic monitorship.

--- app/controllers/monitorships_controller.rb
+++ app/controllers/monitorships_controller.rb
@@ -4,18 +4,11 @@ class MonitorshipsController < ApplicationController
   def update
     topic_id = Topic.find_by_permalink(params[:id]).id
     @monitorship = Monitorship.find_or_initialize_by_user_id_and_topic_id(current_user.id, topic_id)
-    @monitorship.update_attribute :active, true
+    @monitorship.update_attribute :active, params.has_key?(:enable_monitor)
     respond_to do |format|
       format.html { redirect_to topic_path(params[:id]) }
 #      format.js
     end
   end
 
-  def destroy
-    Monitorship.update_all ['active = ?', false], ['user_id = ? and topic_id = ?', current_user.id, params[:topic_id]]
-    respond_to do |format|
-      format.html { redirect_to topic_path(params[:forum_id], params[:topic_id]) }
-#      format.js
-    end
-  end
 end
--- spec/controllers/monitorships_controller_spec.rb
+++ spec/controllers/monitorships_controller_spec.rb
@@ -1,2 +1,26 @@
+require File.dirname(__FILE__) + '/../spec_helper'
 
-# TODO
+describe MonitorshipsController, "PUT #update" do
+  define_models
+
+  before do
+    login_as :default
+    @topic = mock_model(Topic)
+    @monitorship = mock_model(Monitorship)
+    Topic.stub!(:find_by_permalink).with("topic-name").and_return(@topic)
+    Monitorship.stub!(:find_or_initialize_by_user_id_and_topic_id).and_return(@monitorship)
+  end
+
+  it "should enable monitorship for a post, for a user" do
+    @monitorship.should_receive(:update_attribute).with(:active, true)
+    put :update, :id => 'topic-name', :enable_monitor => 'on'
+    assigns[:monitorship].should == @monitorship
+  end
+
+  it "should disable monitorship for a post, for a user" do
+    @monitorship.should_receive(:update_attribute).with(:active, false)
+    put :update, :id => 'topic-name'
+    assigns[:monitorship].should == @monitorship
+  end
+
+end
