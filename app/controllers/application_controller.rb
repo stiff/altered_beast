50c283579b382052b4056b1b6cb8152e86c90ba4
Gabriel Oliveira <gabriel.so@gmail.com>
2010-07-13 16:15:43 -0300

    voltando com ExceptionNotifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   include AuthenticatedSystem
   include PathHelper
-  # include ExceptionNotification::Notifiable
+  include ExceptionNotification::Notifiable
 
   helper :all
   helper_method :current_page,:can_comment?
