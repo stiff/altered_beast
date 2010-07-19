3fb0d06eb14716699418bd56cfdaa1aa5ca1fc01
Adriano Almeida <adrianoalmeida7@gmail.com>
2010-07-19 14:20:19 -0300

    Iniciando correcao de testes

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -2,11 +2,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ForumsController, "GET #show" do
 
+=begin
   before do
     @forum = mock_model(Forum)
     @topics = mock(Array)
     Forum.stub!(:find_by_permalink).and_return(@forum)
-    @forum.should_receive(:topics).and_return(@topics)
+    @forum.should_receive(:paginate_by_forum_id).and_return(@topics)
     @topics.stub!(:paginate).and_return(@topics)
   end
 
@@ -25,5 +26,17 @@ describe ForumsController, "GET #show" do
     @topics.should_receive(:paginate).with(:page => 5).and_return(@topics)
     get :show, :id => "arquitetura", :page => '5'
   end
+=end
 
+  it "should paginate all topics if show all flag is enabled" do
+    pending
+  end
+  
+  it "should paginate just the topics with score higher than 0 if show all flag is not enabled" do
+    pending
+  end
+  
+  it "should render the show template" do
+    pending
+  end
 end
