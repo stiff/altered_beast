27745f1a7d3bce17fbbac0fa44ca8bc0b34c079f
Pedro Matiello <pmatiello@gmail.com>
2010-08-24 14:10:30 -0300

    Tests for tags as keywords in topic pages meta tag.

--- spec/views/topics/show.html.erb_spec.rb
+++ spec/views/topics/show.html.erb_spec.rb
@@ -3,12 +3,17 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe "/topics/show.html.erb" do
   define_models :topics_controller
   
+  def head_content
+    response.template.instance_variable_get("@content_for_head_content")
+  end
+  
   before do
     @forum  = forums(:default)
     @topic  = topics(:default)
     Forum.stub!(:first).and_return(@forum)
 
     @topic.title = "Topic Title"
+    @topic.tags = [Tag.new(:name => "first tag"), Tag.new(:name => "other tag")]
 
     assigns[:topic] = @topic
     assigns[:posts] = []
@@ -16,7 +21,12 @@ describe "/topics/show.html.erb" do
 
   it "should render post title as meta description" do
   	render "/topics/show.html.erb"
-  	response.template.instance_variable_get("@content_for_head_content").should include('<meta content="Topic title" name="description" />')
+  	head_content.should include('<meta content="Topic title" name="description" />')
+  end
+  
+  it "should render post tags as meta description" do
+  	render "/topics/show.html.erb"
+  	head_content.should include('<meta content="first tag, other tag" name="keywords" />')
   end
 end
 
