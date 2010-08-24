4c902f6839962933b180a83207e20c900653e8d6
Pedro Matiello <pmatiello@gmail.com>
2010-08-24 16:14:41 -0300

    Compressing Javascripts.

--- minify.sh
+++ minify.sh
@@ -3,7 +3,10 @@
 YUI="java -jar misc/yuicompressor.jar"
 
 CSS_PATH="public/stylesheets/"
+JS_PATH="public/javascripts/"
 TEMP="temporary/"
+
+echo "Minifying Stylesheets..."
 
 rm -rf $TEMP
 mkdir -p $TEMP/$CSS_PATH
@@ -13,3 +16,15 @@ find $CSS_PATH -name *.css -exec ${YUI} {} -o $TEMP/{} \;
 
 rm -rf $CSS_PATH/*
 cp $TEMP/$CSS_PATH/* $CSS_PATH
+
+echo "Minifying Javascripts..."
+
+rm -rf $TEMP
+mkdir -p $TEMP/$JS_PATH
+rm -f $JS_PATH/beast.js
+
+find $JS_PATH -name *.js -exec ${YUI} {} -o $TEMP/{} \;
+
+rm -rf $JS_PATH/*
+cp $TEMP/$JS_PATH/* $JS_PATH
+
