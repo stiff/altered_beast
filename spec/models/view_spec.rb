7f8fcc60ba2ce31d30feab5c7949ba4762393803
Pedro Matiello <pmatiello@gmail.com>
2010-11-09 16:53:46 -0200

    A less fragile test for hottest topics.

--- spec/models/view_spec.rb
+++ spec/models/view_spec.rb
@@ -3,6 +3,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Topic, "Hits count" do
   define_models
   before do
+    sleep 1
+    @initTime = 2.days.ago
+    
     @topic1 = topics(:default)
     @topic2 = topics(:other)
     @topic3 = topics(:other_forum)
@@ -16,11 +19,17 @@ describe Topic, "Hits count" do
   end
   
   it "top hottest topics" do
-    hottest_topics = Topic.top_hottest_since(2.days.ago, 2)
+    hottest_topics = Topic.top_hottest_since(@initTime, 2)
     
     hottest_topics.size.should == 2
-    hottest_topics.first.should == @topic2
-    hottest_topics.last.should == @topic3
+    hottest_topics.should include(@topic2)
+    hottest_topics.should include(@topic3)
+  end
+  
+  it "should produce a new view for every hit on a topic" do
+    views = View.all.size
+    @topic1.hit!
+    View.all.size.should == views+1
   end
   
   it "should send an email with the latest hottest topics" do
