afe4da29bfa43cf478145ec61ea46d33510246c4
Pedro Matiello <pmatiello@gmail.com>
2010-11-09 16:54:55 -0200

    Made user email visible to admin users.

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/users/show.html.erb" do
+  define_models :users_controller
+  
+  before do
+    @user = users(:default)
+    @user.signature = "A very provocative signature"
+    @user.signature_html = "A very provocative signature"
+    
+    @admin = mock_model(User)
+    @admin.stub!('admin?').and_return(true)
+    
+    assigns[:user] = @user
+  end
+
+  it "should show user email to admins" do
+    template.should_receive(:current_user).and_return(@admin)
+    render "/users/show.html.erb"
+    response.body.should include("normal-user")
+    response.body.should include("normal-user@example.com")
+  end
+  
+  it "should not show user email to non-admins" do
+    template.should_receive(:current_user).and_return(@user)
+    render "/users/show.html.erb"
+    response.body.should include("normal-user")
+    response.body.should_not include("normal-user@example.com")
+  end
+end
+
